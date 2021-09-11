f0279b6bab3470eeddc5a441019abed1a0cf5913
York <YorkAARGH@users.noreply.github.com>
2021-09-11 02:30:14 +0100

    Initial Commit
    Let's get the docs up!

---
+++ api_project.js
@@ -0,0 +1,28 @@
+define({
+  "name": "CoreAPI",
+  "version": "1.0.0",
+  "description": "This is the Core API for Misaki, it constitutes both her economy system as well as her settings.",
+  "title": "CoreAPI",
+  "url": "http://localhost:3030",
+  "order": [
+    "Utilities",
+    "Settings",
+    "Economy",
+    "Ping",
+    "settingsLoad",
+    "settingsSave",
+    "settingsDelete",
+    "profileLoad",
+    "profileSave",
+    "profileDelete"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-09-11T01:16:43.491Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
